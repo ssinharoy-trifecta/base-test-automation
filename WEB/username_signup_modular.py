634bfd57061630c16bbbf52cf4170ad2fc35e75c
sozawa-trifecta <scott.ozawa@trifectanutrition.com>
2021-08-16 16:00:36 -0700

    Fix imports, remove comments
    Removing comments and updating the imports to traverse the folder structure correctly

--- WEB/username_signup_modular.py
+++ WEB/username_signup_modular.py
@@ -1,12 +1,8 @@
-import time, os, sys
-sys.path.append('Common/')
-sys.path.append('Pages/Account')
-sys.path.append('Pages/Auth')
-sys.path.append('Pages/Shop')
-from setupteardown import SeleniumSetupTearDown
-from shop_landing_page import ValidateShopHome, NavigateShop
-from login_page import ValidateLoginPg, NavigateLogin
-from account_home_page import ValidateAcctHome, NavigateAcct
+import time
+from Common.setupteardown import SeleniumSetupTearDown
+from Pages.Shop.shop_landing_page import ValidateShopHome, NavigateShop
+from Pages.Auth.login_page import ValidateLoginPg, NavigateLogin
+from Pages.Account.account_home_page import ValidateAcctHome, NavigateAcct
 
 
 
